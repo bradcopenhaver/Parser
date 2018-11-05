bd1c3eb44f67ad4b1d4bb7b20fa44e38dcaaaf94
Brad <bradcopenhaver@gmail.com>
2018-11-04 21:16:22 -0800

    add custom classes for better data portability

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,51 @@
+function Patient(){
+  var name;
+  var dob;
+  var email;
+  var phone;
+  var documents;
+}
+
+function Document(){
+  var id;
+  var name;
+  var date;
+}
+
 function extract(){
   //Put the pasted html into the DOM
   var rawData = document.getElementById("html-entry").value;
   $("#pasted").html(rawData);
 
-  //Get the patient info and add to a string of html
-  var pulledData = "Name: " + $("a[target='_blank']:eq(0)").text() + "<br>";
-  pulledData += "DOB: " + $("h4").text() + "<br>";
-  pulledData += "Email: " + $("a[target='_blank']:eq(1)").text() + "<br>";
-  pulledData += "Phone: " + $("small").html().slice(0, $("small").html().indexOf(">")+1);
+  //Create a new Patient record and add personal info
+  var newPatient = new Patient();
+  newPatient.name = $("a[target='_blank']:eq(0)").text();
+  newPatient.dob = $("h4").text();
+  newPatient.email = $("a[target='_blank']:eq(1)").text();
+  newPatient.phone = $("small").html().slice(0, $("small").html().indexOf("<")-1);
+  newPatient.documents = [];
+
+  //Create Documents and add to newPatient
+  $("tbody:eq(6)").children("tr").each(function(i) {
+    var newDocument = new Document();
+    newDocument.id = $(this).find("option:first-of-type").attr("value").slice(0, $(this).find("option:first-of-type").attr("value").indexOf(","));
+    newDocument.name = $(this).children("td:eq(2)").text();
+    newDocument.date = $(this).children("td:eq(0)").text();
+    newPatient.documents.push(newDocument);
+  });
+
+  //Create html string to display info
+  var pulledData = "Name: " + newPatient.name + "<br>";
+  pulledData += "DOB: " + newPatient.dob + "<br>";
+  pulledData += "Email: " + newPatient.email + "<br>";
+  pulledData += "Phone: " + newPatient.phone + "<br>";
   pulledData += "Labs & Documents <br>";
 
   //Loop through the labs and documents and add them to the html
-  $("tbody:eq(6)").children("tr").each(function(i) {
-    pulledData += "ID: " + $(this).find("option:first-of-type").attr("value").slice(0, $(this).find("option:first-of-type").attr("value").indexOf(",") ) + " <br>";
-    pulledData += "Date: " + $(this).children("td:eq(0)").text() + " <br>";
-    pulledData += "Name: " + $(this).children("td:eq(2)").text() + " <br>";
+  newPatient.documents.forEach(function (document){
+    pulledData += "ID: " + document.id + " <br>";
+    pulledData += "Date: " + document.date + " <br>";
+    pulledData += "Name: " + document.name + " <br>";
   });
 
   //Display the compiled string of info
